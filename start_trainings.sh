ce758a7163862d792d9615cc78703c9df6cb43d5
Axel Chemla--Romeu-Santos <axelchemla@yahoo.fr>
2022-09-26 20:01:06 +0200

    Update start_trainings.sh

--- start_trainings.sh
+++ start_trainings.sh
@@ -1,9 +1,10 @@
-python3 train_model.py --config-name div_alexnet_isfid rundir=/data/axel/neurips 
-python3 train_model.py --config-name div_inception_isfid rundir=/data/axel/neurips 
-python3 train_model.py --config-name div_mobilenet_isfid rundir=/data/axel/neurips 
-python3 train_model.py --config-name div_custom_isfid rundir=/data/axel/neurips 
+RUNDIR="runs/"
+python3 train_model.py --config-name div_alexnet_isfid rundir=${RUNDIR}
+python3 train_model.py --config-name div_inception_isfid rundir=${RUNDIR}
+python3 train_model.py --config-name div_mobilenet_isfid rundir=${RUNDIR} 
+python3 train_model.py --config-name div_custom_isfid rundir=${RUNDIR} 
 
-python3 train_model.py --config-name div_alexnet_mmd rundir=/data/axel/neurips 
-python3 train_model.py --config-name div_inception_mmd rundir=/data/axel/neurips
-python3 train_model.py --config-name div_mobilenet_mmd rundir=/data/axel/neurips
-python3 train_model.py --config-name div_custom_mmd rundir=/data/axel/neurips 
+python3 train_model.py --config-name div_alexnet_mmd rundir=${RUNDIR} 
+python3 train_model.py --config-name div_inception_mmd rundir=${RUNDIR}
+python3 train_model.py --config-name div_mobilenet_mmd rundir=${RUNDIR}
+python3 train_model.py --config-name div_custom_mmd rundir=${RUNDIR} 
